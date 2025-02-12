64e7d746467fcd095abd3aaa8dd61ec33c969934
dawon <aodlstory321@gmail.com>
2025-02-12 19:29:43 +0900

    LV 7 구현

--- src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
@@ -5,4 +5,6 @@ import com.example.schedulejpa.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+
 }
